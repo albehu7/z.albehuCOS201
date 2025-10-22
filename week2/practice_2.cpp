2eed8613682926e3c215b0f7547d509ec05b77f4
zedd-s-repo <levialbehu@gmail.com>
2025-10-22 17:09:35 +0100

    Week2

---
+++ week2/practice_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	// SWAPPIN VARIABLES
+	int a = 5;
+	int b = 2;
+	int temp = a;
+	a = b;
+	b = temp;
+	
+	cout << a << endl;
+	cout << b ;
+	return 0;
+}
